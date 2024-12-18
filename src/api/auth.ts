a222c4e867dc76a9a25084ecd36e4f420389c2c8
trungtran <baotrung06092003@gmail.com>
2024-12-18 15:08:51 +0700

    fix: build

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,7 @@ import apolloAxios from './request';
 export const signIn = async (
   username: string,
   password: string
-): Promise<any> => {
+): Promise<{ user: string }> => {
   const res = await apolloAxios.post('/auth/sign-in', {
     username,
     password,
